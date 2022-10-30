c3daf0079a29b0a10a19955ff0ed2acb055498c4
Max Metral <max@pyralis.com>
2022-10-29 22:54:02 -0400

    Next.js updates to tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import config from '@gasbuddy/coconfig';
 
+Object.assign(config['tsconfig.json'].configuration.compilerOptions, {
+  allowJs: true,
+  skipLibCheck: true,
+  forceConsistentCasingInFileNames: true,
+  noEmit: false,
+  resolveJsonModule: true,
+  jsx: true,
+});
+
 export default {
   ...config,
   'next.config.js': {
